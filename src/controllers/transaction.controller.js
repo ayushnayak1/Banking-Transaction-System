ef9fcbcb87a0105cb8255db2c3a3f859aee23745
Ayush Kumar Nayak <codeitverse@gmail.com>
2026-02-22 01:11:54 +0530

    Update transaction.controller.js

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -4,20 +4,7 @@ const accountModel = require("../models/account.model")
 const emailService = require("../services/email.service")
 const mongoose = require("mongoose")
 
-/**
- * - Create a new transaction
- * THE 10-STEP TRANSFER FLOW:
-     * 1. Validate request
-     * 2. Validate idempotency key
-     * 3. Check account status
-     * 4. Derive sender balance from ledger
-     * 5. Create transaction (PENDING)
-     * 6. Create DEBIT ledger entry
-     * 7. Create CREDIT ledger entry
-     * 8. Mark transaction COMPLETED
-     * 9. Commit MongoDB session
-     * 10. Send email notification
- */
+
 
 async function createTransaction(req, res) {
 
